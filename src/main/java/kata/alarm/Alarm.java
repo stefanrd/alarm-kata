5180baeba9130510ca3814ca24f54ca4aab1142f
Stefan Radosavljevic <radosavljevic.stefan@live.com>
2020-04-28 22:51:02 +0200

    Replaced current time in checkForAlarm with time provided by localTimeProvider

--- src/main/java/kata/alarm/Alarm.java
+++ src/main/java/kata/alarm/Alarm.java
@@ -2,11 +2,19 @@ package kata.alarm;
 
 import java.time.LocalTime;
 
-public class Alarm {
+import kata.localtime.ILocalTimeProvider;
+import kata.localtime.LocalTimeProvider;
 
+public class Alarm {
+  private final ILocalTimeProvider localTimeProvider;
+  
+  public Alarm() {
+	  localTimeProvider = new LocalTimeProvider();
+  }
+  
   public int checkForAlarm() {
 
-    final LocalTime now = LocalTime.now();
+    final LocalTime currentTime = localTimeProvider.getCurrentTime();
 
     final int elapsed = (now.getHour() * 60 ) + now.getMinute();
 
